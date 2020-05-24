ae8b08c33aeb16da5fd7232f9d49e734ab8d5dfe
Tomáš Kuča <tomas@kuca.cz>
2020-05-24 17:19:13 +0200

    Cleanup code

--- lib/negative-captcha.rb
+++ lib/negative-captcha.rb
@@ -1,13 +1,9 @@
+# frozen_string_literal: true
+
 require 'i18n'
 ::I18n.load_path += Dir[File.expand_path('../config/locales/**/*.{rb,yml}', __dir__)]
 
-if RUBY_VERSION.to_f >= 1.9
-  RUBY_19 = true
-  require 'digest/md5'
-else
-  RUBY_19 = false
-  require 'md5'
-end
+require 'digest/md5'
 
 class NegativeCaptcha
   attr_accessor :fields
@@ -19,24 +15,17 @@ class NegativeCaptcha
   attr_accessor :error
 
   def initialize(opts)
-    @secret = opts[:secret]||(RUBY_19 ? Digest::MD5.hexdigest("this_is_a_secret_key") : MD5.hexdigest("this_is_a_secret_key"))
-    @timestamp =  (opts.has_key?(:params) ? opts[:params][:timestamp] : nil) || Time.now.to_i
-    spinner_text = ([@timestamp, @secret] + (opts[:spinner].is_a?(Array) ? opts[:spinner] : [opts[:spinner]]))*'-'
-    @spinner = RUBY_19 ? Digest::MD5.hexdigest(spinner_text) : MD5.hexdigest(spinner_text)
-    @message = opts[:message]|| I18n.t(:message, scope: [:negative_captcha, :errors])
-    @fields = opts[:fields].inject({}){ |hash, field_name|
-      hash[field_name] = \
-      if RUBY_19
-        Digest::MD5.hexdigest([field_name, @spinner, @secret]*'-')
-      else
-        MD5.hexdigest([field_name, @spinner, @secret]*'-')
-      end
-
-      hash
-    }
+    @secret = opts[:secret]
+    @timestamp = (opts.key?(:params) ? opts[:params][:timestamp] : nil) || Time.now.to_i
+    spinner_text = ([@timestamp, @secret] + (opts[:spinner].is_a?(Array) ? opts[:spinner] : [opts[:spinner]])) * '-'
+    @spinner = Digest::MD5.hexdigest(spinner_text)
+    @message = opts[:message] || I18n.t(:message, scope: [:negative_captcha, :errors])
+    @fields = opts[:fields].each_with_object({})  do |field_name, hash|
+      hash[field_name] = Digest::MD5.hexdigest([field_name, @spinner, @secret].join('-'))
+    end
     @values = {}
     @error = I18n.t(:no_params, scope: [:negative_captcha, :errors])
-    process(opts[:params]) if opts[:params] && (opts[:params][:spinner]||opts[:params][:timestamp])
+    process(opts[:params]) if opts[:params] && (opts[:params][:spinner] || opts[:params][:timestamp])
   end
 
   def [](name)
@@ -44,7 +33,7 @@ class NegativeCaptcha
   end
 
   def valid?
-    @error.nil? || @error == "" || @error.empty?
+    @error.nil? || @error == '' || @error.empty?
   end
 
   def assign_values(params)
@@ -54,22 +43,22 @@ class NegativeCaptcha
   end
 
   def process(params)
-    if params[:timestamp].nil? || (Time.now.to_i - params[:timestamp].to_i).abs > 86400
+    if params[:timestamp].nil? || (Time.now.to_i - params[:timestamp].to_i).abs > 86_400
       @error = I18n.t(:invalid_timestamp, scope: [:negative_captcha, :errors])
     elsif params[:spinner] != @spinner
       @error = I18n.t(:invalid_spinner, scope: [:negative_captcha, :errors])
-    elsif fields.keys.detect {|name| params[name] && params[name].length > 0}
+    elsif fields.keys.detect { |name| params[name] && !params[name].empty? }
       @error = I18n.t(:hidden_fields_submitted, scope: [:negative_captcha, :errors])
       false
     else
-      @error = ""
+      @error = ''
       assign_values(params)
     end
   end
 end
 
-if !ActionView::Base.instance_methods.include? 'negative_captcha'
+unless ActionView::Base.instance_methods.include? 'negative_captcha'
   require 'negative_captcha_view_helpers'
   ActionView::Base.class_eval { include NegativeCaptchaViewHelpers }
 end
-require "negative_captcha_form_builder"
+require 'negative_captcha_form_builder'

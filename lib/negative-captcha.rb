18fa7294c891df8954110d65c197e91d78fb9e36
Tomáš Kuča <tomas@kuca.cz>
2020-05-24 17:17:06 +0200

    Use i18n gem for error messages

--- lib/negative-captcha.rb
+++ lib/negative-captcha.rb
@@ -1,3 +1,6 @@
+require 'i18n'
+::I18n.load_path += Dir[File.expand_path('../config/locales/**/*.{rb,yml}', __dir__)]
+
 if RUBY_VERSION.to_f >= 1.9
   RUBY_19 = true
   require 'digest/md5'
@@ -20,7 +23,7 @@ class NegativeCaptcha
     @timestamp =  (opts.has_key?(:params) ? opts[:params][:timestamp] : nil) || Time.now.to_i
     spinner_text = ([@timestamp, @secret] + (opts[:spinner].is_a?(Array) ? opts[:spinner] : [opts[:spinner]]))*'-'
     @spinner = RUBY_19 ? Digest::MD5.hexdigest(spinner_text) : MD5.hexdigest(spinner_text)
-    @message = opts[:message]||"Please try again. This usually happens because an automated script attempted to submit this form."
+    @message = opts[:message]|| I18n.t(:message, scope: [:negative_captcha, :errors])
     @fields = opts[:fields].inject({}){ |hash, field_name|
       hash[field_name] = \
       if RUBY_19
@@ -32,7 +35,7 @@ class NegativeCaptcha
       hash
     }
     @values = {}
-    @error = "No params provided"
+    @error = I18n.t(:no_params, scope: [:negative_captcha, :errors])
     process(opts[:params]) if opts[:params] && (opts[:params][:spinner]||opts[:params][:timestamp])
   end
 
@@ -46,11 +49,11 @@ class NegativeCaptcha
 
   def process(params)
     if params[:timestamp].nil? || (Time.now.to_i - params[:timestamp].to_i).abs > 86400
-      @error = "Error: Invalid timestamp.  #{@message}"
+      @error = I18n.t(:invalid_timestamp, scope: [:negative_captcha, :errors])
     elsif params[:spinner] != @spinner
-      @error = "Error: Invalid spinner.  #{@message}"
+      @error = I18n.t(:invalid_spinner, scope: [:negative_captcha, :errors])
     elsif fields.keys.detect {|name| params[name] && params[name].length > 0}
-      @error = "Error: Hidden form fields were submitted that should not have been.  #{@message}"
+      @error = I18n.t(:hidden_fields_submitted, scope: [:negative_captcha, :errors])
       false
     else
       @error = ""
